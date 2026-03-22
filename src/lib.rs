9929752dc9c093c038eb85e8979cc6fe043788fc
Clémentine Scohy <clembytes@proton.me>
2026-03-22 18:44:22 +0100

    Made insertion_sort generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub fn insertion_sort(a: &mut [i64]) {
+pub fn insertion_sort<T: std::cmp::PartialOrd + Copy>(a: &mut [T])  {
     let mut end = 0;
     while end < a.len() - 1 {
         let new_value = a[end + 1];
@@ -30,4 +30,16 @@ fn test_insertion_sort() {
     let mut a = [1, 3, 2, 4, 6, 5, 8, 7, 9, 0];
     insertion_sort(&mut a);
     assert_eq!(a, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    let mut a = [-5, 5, -3, 0, -1, 9, 42];
+    insertion_sort(&mut a);
+    assert_eq!(a, [-5, -3, -1, 0, 5, 9, 42]);
+
+    let mut a = [std::f64::consts::PI, 3.14, -6.18, 0., 42., -1., f64::INFINITY, -f64::INFINITY];
+    insertion_sort(&mut a);
+    assert_eq!(a, [-f64::INFINITY, -6.18, -1.0, 0.0, 3.14, std::f64::consts::PI, 42.0, f64::INFINITY]);
+
+    let mut a = ["1", "3", "2", "4", "6", "5", "8", "7", "AZ", "9", "0", "12", "az"];
+    insertion_sort(&mut a);
+    assert_eq!(a, ["0", "1", "12", "2", "3", "4", "5", "6", "7", "8", "9", "AZ", "az"]);
 }
