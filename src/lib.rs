22e0ea789548c5fd633dfb15576c092eb94bc1ab
Clémentine Scohy <clembytes@proton.me>
2026-03-22 19:48:56 +0100

    Use macro to reuse same tests for different sorts

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub fn insertion_sort<T: std::cmp::PartialOrd>(a: &mut [T])  {
+pub fn insertion_sort<T: std::cmp::PartialOrd>(a: &mut [T]) {
     let mut end = 0;
     while end < a.len() - 1 {
         for i in 0..=end {
@@ -11,29 +11,73 @@ pub fn insertion_sort<T: std::cmp::PartialOrd>(a: &mut [T])  {
     }
 }
 
+#[cfg(test)]
+macro_rules! test_any_sort {
+    ( $f:ident ) => {
+        let mut a = [45, 10, 34];
+        $f(&mut a);
+        assert_eq!(a, [10, 34, 45]);
+
+        let mut a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+        $f(&mut a);
+        assert_eq!(a, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        let mut a = [1, 3, 2, 4, 6, 5, 8, 7, 9, 0];
+        $f(&mut a);
+        assert_eq!(a, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        let mut a = [-5, 5, -3, 0, -1, 9, 42];
+        $f(&mut a);
+        assert_eq!(a, [-5, -3, -1, 0, 5, 9, 42]);
+
+        let mut a = [
+            std::f64::consts::PI,
+            3.14,
+            -6.18,
+            0.,
+            42.,
+            -1.,
+            f64::INFINITY,
+            -f64::INFINITY,
+        ];
+        $f(&mut a);
+        assert_eq!(
+            a,
+            [
+                -f64::INFINITY,
+                -6.18,
+                -1.0,
+                0.0,
+                3.14,
+                std::f64::consts::PI,
+                42.0,
+                f64::INFINITY
+            ]
+        );
+
+        let mut a = [
+            "1", "3", "2", "4", "6", "5", "8", "7", "AZ", "9", "0", "12", "az",
+        ];
+        $f(&mut a);
+        assert_eq!(
+            a,
+            [
+                "0", "1", "12", "2", "3", "4", "5", "6", "7", "8", "9", "AZ", "az"
+            ]
+        );
+
+        let mut a: Vec<String> = a.into_iter().map(String::from).collect();
+        $f(&mut a);
+        assert_eq!(
+            a,
+            [
+                "0", "1", "12", "2", "3", "4", "5", "6", "7", "8", "9", "AZ", "az"
+            ]
+        );
+    };
+}
+
 #[test]
 fn test_insertion_sort() {
-    let mut a = [45, 10, 34];
-    insertion_sort(&mut a);
-    assert_eq!(a, [10, 34, 45]);
-
-    let mut a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-    insertion_sort(&mut a);
-    assert_eq!(a, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
-    let mut a = [1, 3, 2, 4, 6, 5, 8, 7, 9, 0];
-    insertion_sort(&mut a);
-    assert_eq!(a, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
-    let mut a = [-5, 5, -3, 0, -1, 9, 42];
-    insertion_sort(&mut a);
-    assert_eq!(a, [-5, -3, -1, 0, 5, 9, 42]);
-
-    let mut a = [std::f64::consts::PI, 3.14, -6.18, 0., 42., -1., f64::INFINITY, -f64::INFINITY];
-    insertion_sort(&mut a);
-    assert_eq!(a, [-f64::INFINITY, -6.18, -1.0, 0.0, 3.14, std::f64::consts::PI, 42.0, f64::INFINITY]);
-
-    let mut a = ["1", "3", "2", "4", "6", "5", "8", "7", "AZ", "9", "0", "12", "az"];
-    insertion_sort(&mut a);
-    assert_eq!(a, ["0", "1", "12", "2", "3", "4", "5", "6", "7", "8", "9", "AZ", "az"]);
+    test_any_sort!(insertion_sort);
 }
